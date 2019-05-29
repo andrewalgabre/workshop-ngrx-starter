b094dabb042298e96a45e646b3779e6f5784e0ee
Andrew Algabre <a.algabre@me.com>
2019-05-29 10:04:28 +0200

    routes

--- src/app/layout/containers/layout/layout.component.ts
+++ src/app/layout/containers/layout/layout.component.ts
@@ -10,9 +10,9 @@ import { Observable } from 'rxjs';
 export class LayoutComponent implements OnInit {
   menuItems = [
     {
-      text: 'Home',
-      route: '/',
-      name: 'Home',
+      text: 'Counter',
+      route: '/counter',
+      name: 'Counter',
       icon: 'home'
     },
     {
