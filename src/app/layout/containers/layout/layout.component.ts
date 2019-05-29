dd562151335a94b0dbdf1a98f266e33464fa6cb2
Andrew Algabre <a.algabre@me.com>
2019-05-29 09:59:15 +0200

    sidenav

--- src/app/layout/containers/layout/layout.component.ts
+++ src/app/layout/containers/layout/layout.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
-// import { AppState } from '../../../store/reducers/app.reducer';
-// import { Observable } from 'rxjs';
-// import { LogoutAction } from '../../../auth/store/actions/auth.actions';
-// import { selectLoggedIn } from '../../../auth/store/selectors/auth.selectors';
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
